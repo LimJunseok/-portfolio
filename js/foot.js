8b235ee736aac747ac0d1119107814fc13402c09
LimJunseok <76501234+LimJunseok@users.noreply.github.com>
2021-02-17 17:28:20 +0900

    0217

--- js/foot.js
+++ js/foot.js
@@ -1,4 +1,26 @@
 $(document).mousemove(function(e){
     $('.red-ball').css("top", e.pageY);
     $('.red-ball').css("left", e.pageX);
+    
+    $(function(){
+        $('#pop_bt').click(function(e) {
+        	e.stopPropagation();
+            $('#pop').show(); 
+        })  
+        $(document).click(function(){
+            $('#pop').hide(); 
+        })
+     })
+     $(function(){
+        $('#pop_bt2').click(function(e) {
+        	e.stopPropagation();
+            $('#pop2').show(); 
+        })  
+        $(document).click(function(){
+            $('#pop2').hide(); 
+        })
+     })
+
  });
+
+
